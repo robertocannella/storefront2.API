b95c5b01ec0201c3b860c432a5713f150d455deb
robertocannella <robertocannella@gmail.com>
2024-01-01 22:00:29 -0500

    Add Serialized relationship

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,5 @@
 from decimal import Decimal, ROUND_HALF_UP
-
+from .models import Collection
 from rest_framework import serializers
 
 from store.models import Product
@@ -10,6 +10,8 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=10, decimal_places=2,source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all)
+    collection = serializers.StringRelatedField()
 
     def calculate_tax(self, product):
         tax_rate = Decimal('1.1')  # Using a string to initialize ensures precision
