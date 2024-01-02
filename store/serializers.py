d61c714a7badd2416a8d19bf9201a640bb72160d
robertocannella <robertocannella@gmail.com>
2024-01-01 21:50:33 -0500

    Add custom fields to API endpoint

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,20 @@
+from decimal import Decimal, ROUND_HALF_UP
+
 from rest_framework import serializers
+
+from store.models import Product
 
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2,source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product):
+        tax_rate = Decimal('1.1')  # Using a string to initialize ensures precision
+        calculated_tax = product.unit_price * tax_rate
+        # Round to 3 decimal places
+        return calculated_tax.quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
+
 
