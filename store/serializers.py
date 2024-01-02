ab86682753c0867a352f7b963086cbed898b5850
robertocannella <robertocannella@gmail.com>
2024-01-01 23:14:22 -0500

    Add ModelSerializer Classes

--- store/serializers.py
+++ store/serializers.py
@@ -4,9 +4,10 @@ from rest_framework import serializers
 from store.models import Product
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id','title']
 
 
 class ProductSerializer(serializers.Serializer):
